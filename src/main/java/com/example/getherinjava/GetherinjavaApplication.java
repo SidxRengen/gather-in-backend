ddfc27269276e409f0023cd72cd162d26a9c4711
siddharth <siddharth@meon.co.in>
2026-01-18 23:07:51 +0530

    dot-env file added for the config

--- src/main/java/com/example/getherinjava/GetherinjavaApplication.java
+++ src/main/java/com/example/getherinjava/GetherinjavaApplication.java
@@ -1,11 +1,20 @@
 package com.example.getherinjava;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GetherinjavaApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(GetherinjavaApplication.class, args);
 	}
 }
