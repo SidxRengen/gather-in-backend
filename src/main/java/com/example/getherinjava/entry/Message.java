650a53363c146519c4c7dfb26e0fe7be0fdf187c
siddharth <siddharth@meon.co.in>
2026-03-04 15:44:40 +0530

    database changed to postgres

--- src/main/java/com/example/getherinjava/entry/Message.java
+++ src/main/java/com/example/getherinjava/entry/Message.java
@@ -6,7 +6,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-//@Table(name = "messages")
+@Table(name = "messages")
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
